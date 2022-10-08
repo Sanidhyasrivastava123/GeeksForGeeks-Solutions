55fa5d6e46d42b2103dd708ff23d37f37c230d94
Sanidhya Srivastava <80981040+Sanidhyasrivastava123@users.noreply.github.com>
2022-10-08 10:35:29 +0530

    Add files via upload

---
+++ kthSmallestElement.java
@@ -0,0 +1,11 @@
+//User function Template for Java
+
+class Solution{
+    public static int kthSmallest(int[] arr, int l, int r, int k) 
+    { 
+        //Your code here
+        Arrays.sort(arr);
+        return arr[k-1];
+        
+    } 
+}
