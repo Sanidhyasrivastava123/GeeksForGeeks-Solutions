a00c189a197ecaaf045168ba236e554a73efe713
Sanidhya Srivastava <80981040+Sanidhyasrivastava123@users.noreply.github.com>
2022-10-10 02:24:55 +0530

    Update unionOf2Arrays.java

--- Arrays/unionOf2Arrays.java
+++ Arrays/unionOf2Arrays.java
@@ -1,4 +1,4 @@
-
+For Union of 2 Arrays:
 class Solution{
     public static int doUnion(int a[], int n, int b[], int m) 
     {
@@ -14,5 +14,38 @@ class Solution{
         }
         
         return hs.size();
+    }
+}
+
+
+
+For Intersection of 2 Arrays:
+
+
+class Solution
+{
+    public int[] intersection(int[] A, int[] B)
+	{
+        Set<Integer> setA = new HashSet<>();
+        Set<Integer> setAB = new HashSet<>();
+		int i = 0;
+
+        for(int n : A)
+			setA.add(n);
+
+        for(int n : B)
+		{
+            if(setA.contains(n))
+			{
+                setAB.add(n);
+            }
+        }
+
+        int[] result = new int[setAB.size()];
+
+		for(int n : setAB)
+            result[i++] = n;
+
+        return result;
     }
 }
