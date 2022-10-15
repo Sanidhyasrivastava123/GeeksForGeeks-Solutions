6c095bce670482ebafac8c6545d8608b1ba8c2fd
Sanidhya Srivastava <80981040+Sanidhyasrivastava123@users.noreply.github.com>
2022-10-15 23:05:23 +0530

    Add files via upload

---
+++ Mathematics/PrimeNumber.java
@@ -0,0 +1,12 @@
+class Solution{
+    static int isPrime(int n){
+    // code here
+        if(n==1) return 0;
+        for(int i =2;i*i<=n;i++){
+        if(n%i==0){
+        return 0;
+        }
+        } return 1;
+    
+    }
+}
